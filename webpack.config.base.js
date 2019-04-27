7c44b2ab46c56166701e3e263e93b6f8617e0c51
Sudarshan Shenoy <shenoy.sudarshan@gmail.com>
2019-04-27 17:46:09 +0530

    Separate webpack config for development and production

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -3,7 +3,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
