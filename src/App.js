20da58130ff169aeb560bed8e8bfedff96d07d0b
Sudarshan Shenoy <shenoy.sudarshan@gmail.com>
2019-04-28 14:44:43 +0530

    resuse the scripts

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World!!!</h1>
+        <h1>Hello World</h1>
         <h2>Count: {this.state.count}</h2>
         <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
         <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
