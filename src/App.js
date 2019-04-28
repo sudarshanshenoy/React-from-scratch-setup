88730768ceb05d1668fdc2c0e1f4fb7583a62834
Sudarshan Shenoy <shenoy.sudarshan@gmail.com>
2019-04-28 14:35:59 +0530

    Add hot reload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {hot} from 'react-hot-loader';
 
 class App extends React.Component {
   state = {
@@ -7,7 +8,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Hello World!!!</h1>
         <h2>Count: {this.state.count}</h2>
         <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
         <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
@@ -16,4 +17,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default hot(module)(App);
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -16,8 +16,16 @@ module.exports = {
         exclude: /node_modules/,
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react'],
-          plugins: ['@babel/plugin-proposal-class-properties']
+          plugins: [
+            'react-hot-loader/babel',
+            '@babel/plugin-proposal-class-properties'
+          ]
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/
       }
     ]
   },
