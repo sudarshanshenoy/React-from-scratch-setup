a4820b5c2cbcd15af6621ec4f10787bbe23189f9
Sudarshan Shenoy <shenoy.sudarshan@gmail.com>
2019-05-04 10:32:46 +0530

    add jest complete

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,25 +13,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [[
-            '@babel/preset-env',
-            {
-              targets: [
-                'last 2 versions',
-                'not dead',
-                'not < 2%'
-              ],
-              useBuiltIns: 'entry'
-            }
-          ], '@babel/preset-react'],
-          plugins: [
-            'react-hot-loader/babel',
-            '@babel/plugin-proposal-class-properties',
-            '@babel/plugin-syntax-dynamic-import',
-          ]
-        }
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
